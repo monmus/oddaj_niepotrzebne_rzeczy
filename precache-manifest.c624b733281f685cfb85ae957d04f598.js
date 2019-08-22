4346db70222cb158306636195218550ba80d5a77
twój login do GitHuba <twj emailmonikmusial@gmail.com>
2019-08-22 21:01:33 +0200

    Updates

---
+++ precache-manifest.c624b733281f685cfb85ae957d04f598.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15b5a703affa6e2f7c82ca824489ac7",
+    "url": "/oddaj_niepotrzebne_rzeczy/index.html"
+  },
+  {
+    "revision": "4896f2338b8651e5357b",
+    "url": "/oddaj_niepotrzebne_rzeczy/static/css/main.99cdb27a.chunk.css"
+  },
+  {
+    "revision": "54c6e8e2056320e0a198",
+    "url": "/oddaj_niepotrzebne_rzeczy/static/js/2.b55ec96c.chunk.js"
+  },
+  {
+    "revision": "4896f2338b8651e5357b",
+    "url": "/oddaj_niepotrzebne_rzeczy/static/js/main.e4694c61.chunk.js"
+  },
+  {
+    "revision": "8700edc8c829444c77c7",
+    "url": "/oddaj_niepotrzebne_rzeczy/static/js/runtime~main.a6df7e72.js"
+  },
+  {
+    "revision": "798e681bbe663d294a0a9310b9c4327c",
+    "url": "/oddaj_niepotrzebne_rzeczy/static/media/about.798e681b.png"
+  },
+  {
+    "revision": "eda5da16e7bbb1c6c2e6367c5a9c955a",
+    "url": "/oddaj_niepotrzebne_rzeczy/static/media/barrett-ward.eda5da16.jpg"
+  },
+  {
+    "revision": "5690826ab12ade332c7816c1d11051b7",
+    "url": "/oddaj_niepotrzebne_rzeczy/static/media/form-bg.5690826a.jpg"
+  },
+  {
+    "revision": "05aa4f858ba6a292e80804d82b5c4465",
+    "url": "/oddaj_niepotrzebne_rzeczy/static/media/header.05aa4f85.jpg"
+  },
+  {
+    "revision": "1ac211765852812a233eefbf0dc772b0",
+    "url": "/oddaj_niepotrzebne_rzeczy/static/media/rocknwool.1ac21176.jpg"
+  }
+]);
